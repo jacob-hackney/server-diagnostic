1f4509a07ab106ef1f4fdfb0822af923e40af189
jacob-hackney <jdhackney87@gmail.com>
2025-03-03 10:25:22 -0500

    Launch version(1.0.0)

--- README.md
+++ README.md
@@ -7,3 +7,76 @@ npm install server-diagnostic --save
 ```
 
 ## Usage
+
+First, you have to import the library and create an instance of the ServerHub class:
+
+```js
+const ServerHub = require("server-diagnostic");
+// or import ServerHub from 'server-diagnostic';
+const myHub = new ServerHub();
+```
+
+### Class constructor
+
+There are two optional parameters for the ServerHub constructor:
+
+- useRestarter (boolean, default false)
+- restartInterval (integer, default 10000)
+  If useRestarter is set to false, you cannot specifya value for restartInterval. When useRestarter is enabled, the servers added to the hub will automatically restart if they crash every \[restartInterval\] milliseconds.
+
+### Add server method
+
+This method will save a server to the hub you created. It has two parameters:
+
+- filePath (string)
+- url (string)
+  Example:
+
+```js
+const myHub = new ServerHub();
+myHub.addServer("server.js", "http://localhost:3000");
+```
+
+### Remove server method
+
+This is just the opposite of the addServer method. There is not a URL parameter, just a filePath parameter for this method. \
+Example:
+
+```js
+myHub.addServer("server.js", "http://localhost:3000");
+myHub.removeServer("server.js");
+```
+
+### Log diagnostics method
+
+This method will do a special log for the diagnostics of the server. It will log for each server:
+
+- The URL(italicized)
+- Status(Online/Offline)
+- Your ping to it
+  This method is simple enough, with no parameters:
+
+```js
+myHub.logDiagnostics();
+```
+
+### Get servers method
+
+This method will return an array of all of the servers currently stored in your hub:
+
+```js
+const myHub = new ServerHub();
+
+myHub.addServer("server.js", "http://localhost:3000");
+myHub.addServer("server2.js", "http://localhost:3001");
+
+console.log(myHub.getServers);
+```
+
+## Dependencies
+
+- ansi-escape-sequences @6.2.4
+
+## Credits
+
+This whole library was created by Jacob Hackney.
---
+++ index.js
@@ -0,0 +1,109 @@
+import ansi from "ansi-escape-sequences";
+import child_process from "child_process";
+import { promises as fs } from "fs";
+import path from "path";
+
+class ServerHub {
+  constructor(useRestarter = false, restartInterval) {
+    this.useRestarter = useRestarter;
+    this.restartInterval = restartInterval ? restartInterval : 10000;
+    if (!useRestarter && restartInterval) {
+      throw new Error(
+        "Cannot set restart interval without enabling restarter."
+      );
+    }
+    this.#initialize();
+  }
+  async addServer(filePath, url) {
+    await fs.access(filePath);
+    if (path.extname(filePath) !== ".js") {
+      throw new Error("Invalid file type. Only .js files are allowed.");
+    } else {
+      const server = [filePath, url];
+      let exists = false;
+      for (let server of this.#servers) {
+        if (server[0] === filePath) {
+          exists = true;
+          break;
+        }
+        if (server[1] === url) {
+          exists = true;
+          break;
+        }
+      }
+      if (!exists) {
+        this.#servers.push(server);
+      } else {
+        throw new Error(
+          "Server with the same file path or URL already exists on this hub."
+        );
+      }
+    }
+    return this;
+  }
+  async removeServer(filePath) {
+    await fs.access(filePath);
+    this.#servers = this.servers.filter((server) => server[0] !== filePath);
+    return this;
+  }
+  async logDiagnostics() {
+    console.log(ansi.format("[SERVER DIAGNOSTIC LOG]", ["bold", "cyan"]));
+    for (let server of this.#servers) {
+      await this.#logSingleServerDiagnostic(server);
+      console.log(ansi.format("-".repeat(50), ["bold", "cyan"]));
+    }
+    return this;
+  }
+  getServers() {
+    return this.#servers;
+  }
+  #initialize() {
+    for (let server of this.#servers) {
+      child_process.exec(`node ${server[0]}`);
+    }
+    if (this.useRestarter) {
+      setInterval(this.#checkServerCrash(), this.restartInterval);
+    }
+  }
+  async #checkServerCrash() {
+    for (let server of this.#servers) {
+      fetch(`${server[1]}/testpath`).catch(() => {
+        console.warn(`Server at ${server[1]} has crashed. Restarting...`);
+        child_process
+          .exec(`node ${server[0]}`)
+          .then(() =>
+            console.log(`Server at ${server[1]} has been restarted.`)
+          );
+      });
+    }
+  }
+  async #logSingleServerDiagnostic(server) {
+    console.log(ansi.format(server[1], ["italic"]));
+    const start = performance.now();
+    try {
+      await fetch(`${server[1]}/testpath`).catch(() => {
+        throw new Error("skip to catch block");
+      });
+      const end = performance.now();
+      const ping = end - start;
+      process.stdout.write("Status: ");
+      console.log(ansi.format("Online", ["green", "bold"]));
+      process.stdout.write("Ping: ");
+      if (ping < 100) {
+        console.log(ansi.format(`${ping} ms`, ["green", "bold"]));
+      } else if (ping < 200) {
+        console.log(ansi.format(`${ping} ms`, ["yellow", "bold"]));
+      } else {
+        console.log(ansi.format(`${ping} ms`, ["red", "bold"]));
+      }
+    } catch {
+      process.stdout.write("Status: ");
+      console.log(ansi.format("Offline", ["red", "bold"]));
+      process.stdout.write("Ping: ");
+      console.log(ansi.format("N/A", ["magenta", "bold"]));
+    }
+  }
+  #servers = [];
+}
+
+export default ServerHub;
